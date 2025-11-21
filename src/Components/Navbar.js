32a4d71554bf4cc52ad3d6f92016482b556e03c7
52131378-byte <52131378@students.liu.edu.lb>
2025-11-21 18:23:56 +0200

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar(){
+    return(
+        <nav className="navbar">
+<h1 className="title" >SmileBar</h1>
+<div className="links">
+    <Link to= "/">Home</Link>
+    <Link to= "/products">Products</Link>
+    <Link to= "/contact">ContactUs</Link>
+    <Link to= "/cart" className="cart-link">Cart ({})</Link>
+</div>
+        </nav>
+    )
+}
