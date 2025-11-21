7e3cfca24164dea92cada08ccf04cc55fa4789bc
52131378-byte <52131378@students.liu.edu.lb>
2025-11-21 21:12:13 +0200

    i fix something in Navbar , and I creact the cart context to make all the card button in mode like in live

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,19 +1,18 @@
 import { Link } from "react-router-dom";
-import "../Assets/Navbar.css"
-export default function Navbar(){
-    return(
-        <nav className="navbar">
-<h1 className="title" >SmileBar</h1>
-<img src="/logo.png" alt="" className="smile-logo"/>
-<div className="links">
-    <Link to= "/">Home</Link>
-    <Link to= "/products">Products</Link>
-    <Link to= "/contact">ContactUs</Link>
-    <Link to= "/cart" className="cart-link">🛒Cart ({})</Link>
-</div>
-
-
-<h2></h2>
-        </nav>
-    )
+import "../Assets/Navbar.css";
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 className="title">SmileBar</h1>
+      <img src="/logo.png" alt="" className="smile-logo" />
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/contact">ContactUs</Link>
+        <Link to="/cart" className="cart-link">
+          🛒Cart ({cartItem.length})
+        </Link>
+      </div>
+    </nav>
+  );
 }
