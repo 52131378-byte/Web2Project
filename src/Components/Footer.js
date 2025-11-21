08b68e8073c60bc3d0c4b585d87bbd8a1a9061b5
52131378-byte <52131378@students.liu.edu.lb>
2025-11-21 19:40:40 +0200

    footer jscript with icon

---
+++ src/Components/Footer.js
@@ -0,0 +1,20 @@
+import {FaInstagram , FaFacebbok , FaWhatsapp} from 'react-icons/fa'
+export default function Footer(){
+    return(
+        <footer className="footer">
+            <div className="footer-content">
+                <div className="social-icons">
+                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                    <FaInstagram/>
+                    </a>
+                     <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                     <FaFacebbok/>
+                     </a>
+                      <a href="https://www.wa.me/1234567890.com" target="_blank" rel="noopener noreferrer">
+                      <FaWhatsapp/>
+                      </a>
+                </div>
+            </div>
+        </footer>
+    )
+}
