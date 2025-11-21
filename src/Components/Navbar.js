6885301b837fbfa465ddbce7219e97d4814d2f6d
52131378-byte <52131378@students.liu.edu.lb>
2025-11-21 20:19:08 +0200

    i add the css for the navbar and i add my image for the logo and my cart

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
-
+import "../Assets/Navbar.css"
 export default function Navbar(){
     return(
         <nav className="navbar">
 <h1 className="title" >SmileBar</h1>
+<img src="/logo.png" alt="" className="smile-logo"/>
 <div className="links">
     <Link to= "/">Home</Link>
     <Link to= "/products">Products</Link>
     <Link to= "/contact">ContactUs</Link>
-    <Link to= "/cart" className="cart-link">Cart ({})</Link>
+    <Link to= "/cart" className="cart-link">🛒Cart ({})</Link>
 </div>
 
 
