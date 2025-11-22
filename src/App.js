dd30f2d9d3abb84cf76e8fea93099ce051ee65c6
52131378-byte <52131378@students.liu.edu.lb>
2025-11-22 02:33:12 +0200

    creat my app with including all my import with local and make all the routers and route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import { BrowserRouter as Router , Routes , Route  } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import Home from "./Pages/Home";
+import Products from "./Pages/Products"; 
+import Contact from "./Pages/Contact";
+import { CartProvider } from "./context/CartContext";
+import Cart from "./Pages/Cart";
+
 function App(){
   return (
+    <CartProvider>
     <Router>
       <Navbar/>
+      <div style={{ minHeight : "80vh"}}>
+        <Routes>
+          < Route path="/"element={<Home/>}/>
+          <Route path="/products" element={<Products/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/cart" element={<Cart/>}/>
+
+        </Routes>
+      </div>
     </Router>
-  )
+    </CartProvider>
+  );
 }
+export default App ; 
